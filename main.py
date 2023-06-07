2851cb80bda4d0a61db19c9a2a84aa3d3e675c0b
Faruk Karaman <134199695+FarukKaramanVektorel@users.noreply.github.com>
2023-06-07 21:12:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from Boksor import Boksor
+from Musabaka import Musabaka
+
+ahmet = Boksor("Ahmet", 15, 100, 90, 60)
+ali = Boksor("Ali", 10, 85, 90, 50)
+mac = Musabaka(ahmet, ali, 90, 100)
+mac.roundBasla()
